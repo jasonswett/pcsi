2d8f8fccc484513239b8248836497c46e27671e7
Jason Swett <jason@electricsasquatch.com>
2012-11-08 21:16:39 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pcsi::Application.config.secret_token = '91333534066033a5bfe9bd668210f5dea2df8010188bb8a98ae61c24aed85f614f84b91e7e7fc77f0b2a325ee9a71fef095ede51d3233fb821d4d279af941503'
