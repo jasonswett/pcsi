c3d7577b9b4c0d6f50041153b11b0f5ffa8f17ef
Jason Swett <jason@electricsasquatch.com>
2012-11-11 11:08:07 -0500

    Cucumber scenarios pass.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
-  attr_accessible :code, :description, :name, :price_per_unit, :unit_of_measure_id
+  attr_accessible :code, :description, :name, :price_per_unit, :unit_of_measure
   validates_presence_of :name, :price_per_unit, :code
 end
--- features/step_definitions/product_steps.rb
+++ features/step_definitions/product_steps.rb
@@ -2,9 +2,15 @@ Given /^I am a pharmacist$/ do
 end
 
 When /^I add a product$/ do
-  pending # express the regexp above with the code you wish you had
+  @product_name = "Some Product"
+  visit new_product_path
+  fill_in "Code", :with => "123"
+  fill_in "Name", :with => @product_name
+  fill_in "Price per unit", :with => "50"
+  click_on "Create Product"
 end
 
 Then /^that product should show up in the list of products$/ do
-  pending # express the regexp above with the code you wish you had
+  visit products_path
+  page.should have_content(@product_name)
 end
