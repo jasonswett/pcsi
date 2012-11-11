d0eefacef8ae5b87a24996e2ef40ff40fcdf5b49
Jason Swett <jason@electricsasquatch.com>
2012-11-11 15:40:55 -0500

    You can now use enter to update product quantity.

--- app/assets/javascripts/product_list.js
+++ app/assets/javascripts/product_list.js
@@ -3,17 +3,22 @@ var ProductList = Class.extend({
     this.productQuantityAdjustmentUrl = options.productQuantityAdjustmentUrl;
     this.observeProductQuantityLabelClicks();
     this.observeProductQuantityInputBlurs();
+    this.observeProductQuantityEnterPresses();
   },
 
   observeProductQuantityInputBlurs: function() {
     var self = this;
     $(".editable input").blur(function() {
-      self.updateProductQuantity({
-        productId: $(this).parents("tr").attr("data-product-id"),
-        newQuantity: $(this).val(),
-        previousQuantity: $(this).parents("tr").attr("data-previous-quantity"),
-        row: $(this).parents("tr")
-      });
+      self.updateProductQuantity($(this));
+    });
+  },
+
+  observeProductQuantityEnterPresses: function() {
+    var self = this;
+    $(".editable input").keypress(function(e) {
+      if (e.which == 13) {
+        self.updateProductQuantity($(this));
+      }
     });
   },
 
@@ -24,23 +29,28 @@ var ProductList = Class.extend({
     });
   },
 
-  updateProductQuantity: function(options) {
+  updateProductQuantity: function(target) {
+    var productId = target.parents("tr").attr("data-product-id");
+    var newQuantity = target.val();
+    var previousQuantity = target.parents("tr").attr("data-previous-quantity");
+    var row = target.parents("tr");
+
     var request = $.ajax({
       url: this.productQuantityAdjustmentUrl,
       type: "POST",
       data: {
         product_quantity_adjustment: {
-          product_id: options.productId,
-          quantity: options.newQuantity - options.previousQuantity
+          product_id: productId,
+          quantity: newQuantity - previousQuantity
         }
       },
       dataType: "json"
     });
 
     request.done(function(msg) {
-      options.row.attr("data-previous-quantity", options.newQuantity);
-      options.row.find(".quantity_label").html(options.newQuantity).show();
-      options.row.find("input").hide();
+      row.attr("data-previous-quantity", newQuantity);
+      row.find(".quantity_label").html(newQuantity).show();
+      row.find("input").hide();
     });
   }
 });
